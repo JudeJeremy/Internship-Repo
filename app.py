a603d9a598e497b589fb1ca6cd7ac3066ea41460
JudeJeremy <86058156+JudeJeremy@users.noreply.github.com>
2022-07-02 09:13:39 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,12 @@ app = Flask(__name__)
 def addit(num1, num2):
     return str(int(num1) + int(num2))
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def add_numbers():
     num1 = 200
     num2 = 300
     op = addit(num1, num2)
     return render_template('addNumbers.html', sum=op)
-   
-    
-
-    
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
